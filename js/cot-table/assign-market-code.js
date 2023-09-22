14659857f433df9e104ca2bedc1b2b6725ae16e4
Koyejo <adinlewakoyejo@yahoo.com>
2023-09-22 22:12:03 +0100

    Added function to assign market code

---
+++ js/cot-table/assign-market-code.js
@@ -0,0 +1,24 @@
+const marketCodes = {
+  aud: '232741',
+  cad: '090741',
+  chf: '092741',
+  eur: '099741',
+  gbp: '096742',
+  jpy: '097741',
+  mxn: '095741',
+  nzd: '112741',
+  dxy: '098662',
+  crude_oil: '06765A',
+  xag: '084691',
+  xau: '088691',
+  zar: '122741'
+}
+
+export function assignMarketCode (selectedCurrency) {
+  if (selectedCurrency !== 'Choose a currreny') {
+    const marketCode = marketCodes[selectedCurrency]
+    return marketCode
+  }
+}
+
+assignMarketCode()
